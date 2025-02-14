782e5122f0be3025ab685d38f3b6b8c008b803f2
Justin Malone <jmalone@Mac.home.arpa>
2025-02-13 22:27:22 -0600

    added all the new stuff from class

---
+++ quotes.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+    <title>PHP Test</title></head>
+  <body>
+    <h1>Hello World</h1><br />
+  <?php 
+    $name = 'Mancy';
+    print '<ol>';
+print "<li> $name is my friend.</li>";
+print '<li> $name is my neighbor.</li>';
+
+print "<li>She cried \"Help!\"</li>";
+echo "<li>I", "need", "\$5.00</li>";
+print '</ol>';
+
+  ?>
+</html>
---
+++ template.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>PHP Test</title></head>
+  <body>
+    <h1>Hello World</h1><br />
+  <?php 
+  
+  
+  
+  
+  
+  ?>
+</html>
