b104bc30a08e07f802e8aea25dc5a212ef70661d
Justin Malone <jmalone@Mac.home.arpa>
2025-03-11 21:41:55 -0500

    updates to hangman.php

--- hw4/hman0.php
+++ hw4/hman0.php
@@ -1,7 +1,7 @@
-
 <?php
 // Function to determine which hangman image to display based on wrong guesses
-function getHangmanImage($wrongGuesses) {
+function getHangmanImage($wrongGuesses)
+{
     return "HIMG/H" . $wrongGuesses . ".gif";
 }
 
@@ -12,18 +12,18 @@ $wrongGuesses = isset($_POST['hidRong']) ? (int)$_POST['hidRong'] : 0;
 $gameOver = ($wrongGuesses >= 6);
 $message = "";
 
-// Checking that there's a guess and the game is still going 
+// Checking that there's a guess and the game is still going
 if (!empty($guess) && !$gameOver) {
-    // ctype_alpha() is checking to make sure the string is a character type and we're also checking strlen to make sure the user only inputs 1 char 
+    // ctype_alpha() is checking to make sure the string is a character type and we're also checking strlen to make sure the user only inputs 1 char
     if (strlen($guess) == 1 && ctype_alpha($guess)) {
         // Using the === operator we talked about in class to compare guessedLetters with guess to make sure they're the same type
         if (strpos($guessedLetters, $guess) === false) {
             // This is the string concatination operator that I found I can use to add the user's guess to the guessedLetters variable
             $guessedLetters .= $guess;
-            
+
             // This is Evil Hangman, so every guess is a wrong guess :)
             $wrongGuesses++;
-            
+
             // Check if game over after this guess
             if ($wrongGuesses >= 6) {
                 $gameOver = true;
@@ -50,12 +50,14 @@ for ($i = 0; $i < $wordLength; $i++) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Evil Hangman</title>
 </head>
+
 <body>
     <h1>Evil Hangman</h1><br />
-    
+
     <?php if ($gameOver): ?>
         <p>Game Over! You lost!</p>
         <p>The word was: "NEVER" (You'll never win at evil hangman!)</p>
@@ -63,15 +65,15 @@ for ($i = 0; $i < $wordLength; $i++) {
         <a href="hman0.htm">Play Again</a>
     <?php else: ?>
         <p>Guess a letter, and click 'Guess!'</p>
-        
+
         <?php if (!empty($message)): ?>
             <p style="color: red;"><?= $message ?></p>
         <?php endif; ?>
-        
+
         <p>Word: <?= $displayWord ?></p>
-        
+
         <p>Guessed letters: <?= chunk_split($guessedLetters, 1, " ") ?></p>
-        
+
         <form action="hman0.php" method="POST" name="form1">
             <input type='text' name='txtGuess' value='' maxlength="1" />
             <input type='hidden' name='hidRite' value='' />
@@ -79,8 +81,9 @@ for ($i = 0; $i < $wordLength; $i++) {
             <input type='hidden' name='hidGuessed' value='<?= $guessedLetters ?>' />
             <input type='submit' value='Guess!' name='btnGuess' />
         </form><br>
-        
+
         <img src='<?= $currentImage ?>' border=1><br /><br>
     <?php endif; ?>
 </body>
+
 </html>
