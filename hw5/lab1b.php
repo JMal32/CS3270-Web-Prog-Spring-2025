4bde34c6287a4f8be14e5dee2d700f8e37730888
Justin Malone <jmalone@Mac.home.arpa>
2025-03-23 19:15:18 -0500

    lab1b/c finished

--- hw5/lab1b.php
+++ hw5/lab1b.php
@@ -1,45 +1,47 @@
 <!Doctype html>
 <html>
-  <head>
-    <title>Lab 1b</title>
-  </head>
-  <body>
-    <form method="post">
-      Enter number of columns (2-20): 
-      <input type="number" name="cols" min="2" max="20" value="12">
-      <input type="submit" value="Submit">
-    </form>
-<?php
-    
-    $aryc = array(1 => "");
-    $ary = array(65 => $aryc);
-    $colno = 16;
-    if (isset($_POST["colno"])) {
-      $colno = $_POST["colno"];
+
+<head>
+  <title>Lab 1b</title>
+</head>
+
+<body>
+  <form method="post">
+    Enter number of columns (2-20):
+    <input type="number" name="colno" min="2" max="20" value="<?php echo $colno; ?>">
+    <input type="submit" value="Submit">
+  </form>
+  <?php
+
+  $aryc = array(1 => "");
+  $ary = array(65 => $aryc);
+  $colno = 16;
+  if (isset($_POST["colno"])) {
+    $colno = $_POST["colno"];
+  }
+
+  $ordletstart = ord("A");
+  $ordletend = ord("K");
+
+  for ($r = $ordletstart; $r <= $ordletend; $r++) {
+    for ($c = 1; $c <= $colno; $c++) {
+      $ary[$r][$c] = chr($r) . "-" . $c;
     }
+  }
 
-    $ordletstart = ord("A");
-    $ordletend = ord("K");
-
-    for ($r = $ordletstart; $r <= $ordletend; $r++) {
-      for ($c = 1; $c <= $colno; $c++) {
-        $ary[$r][$c] = chr($r) . "-" . $c;
-      }
-}
-
-    echo '<pre>';
-    for ($r = $ordletstart; $r <= $ordletend; $r++) {
-      for ($c = 1; $c <= $colno; $c++) {
-        echo $ary[$r][$c] .  " ";
-      }
-      echo "<br>";
+  echo '<pre>';
+  for ($r = $ordletstart; $r <= $ordletend; $r++) {
+    for ($c = 1; $c <= $colno; $c++) {
+      echo $ary[$r][$c] .  " ";
     }
-    echo '</pre>';
+    echo "<br>";
+  }
+  echo '</pre>';
 
-    echo "<br><br><br>";
-    echo "The number of elements in \$ary: " . count($ary) . "\n<br>";
-    echo "The rec-total of elements: " , count($ary, COUNT_RECURSIVE), "\n<br>";
+  echo "<br><br><br>";
+  echo "The number of elements in \$ary: " . count($ary) . "\n<br>";
+  echo "The rec-total of elements: ", count($ary, COUNT_RECURSIVE), "\n<br>";
 
 
-?>
-  </body>
+  ?>
+</body>
---
+++ hw5/lab1c.php
@@ -0,0 +1,113 @@
+<!Doctype html>
+<html>
+
+<head>
+    <title>Lab 1c</title>
+</head>
+
+<body>
+    <h2>Automated 2D Array Example With 20 Columns</h2>
+    <?php
+    // Fixed example with 20 columns
+    $fixed_colno = 20;
+    $ordletstart = ord('A');
+    $ordletend = ord('K');
+
+    $aryc = array(1 => '');
+    $ary = array(65 => $aryc);
+
+    echo '<pre>';
+    for ($r = $ordletstart; $r <= $ordletend; $r++) {
+        for ($c = 1; $c <= $fixed_colno; $c++) {
+            $ary[$r][$c] = chr($r) . '-' . $c;
+        }
+    }
+
+    // This is where i start looping through everything to build the correct table
+    for ($r = $ordletstart - 1; $r <= $ordletend + 1; $r++) {
+        // Loop through columns (including header/footer column)
+        for ($c = 0; $c <= $fixed_colno + 1; $c++) {
+            if ($r == $ordletstart - 1 || $r == $ordletend + 1) {
+                // Header/footer row
+                if ($c == 0 || $c == $fixed_colno + 1) {
+                    echo '      ';  // Corner space
+                } else {
+                    echo str_pad($c, 6, ' ', STR_PAD_BOTH);  // Column number
+                }
+            } else if ($c == 0 || $c == $fixed_colno + 1) {
+                // Header/footer column
+                if ($c == 0) {
+                    echo '  ' . chr($r) . '   ';  // This is where I figured out how to space the row headers correctly
+                } else {
+                    echo '   ' . chr($r) . '  ';  // This is for the right side
+                }
+            } else {
+                // I'm tinkering with spacing here to get things to look better
+                // But this is whe cells get built
+                echo str_pad($ary[$r][$c], 6, ' ', STR_PAD_BOTH);
+            }
+        }
+        echo "\n";  // Made a new line after each row
+    }
+    echo '</pre>';
+    ?>
+
+    <h3>Create Your Own Custom Array</h3>
+    <form method="post">
+        Enter number of columns (2-20):
+        <input type="number" name="colno" min="2" max="20" value="<?php echo isset($_POST['colno']) ? $_POST['colno'] : 0; ?>">
+        <input type="submit" value="Submit">
+    </form>
+
+    <?php
+    if (isset($_POST['colno'])) {
+        $colno = $_POST['colno'];
+        echo '<h4>Your Custom Array with ' . $colno . ' Columns</h4>';
+
+        // Reset array for custom display
+        $aryc = array(1 => '');
+        $ary = array(65 => $aryc);
+
+        echo '<pre>';
+        for ($r = $ordletstart; $r <= $ordletend; $r++) {
+            for ($c = 1; $c <= $colno; $c++) {
+                $ary[$r][$c] = chr($r) . '-' . $c;
+            }
+        }
+
+        // This is where i start looping through everything to build the correct table
+        for ($r = $ordletstart - 1; $r <= $ordletend + 1; $r++) {
+            // Loop through columns (including header/footer column)
+            for ($c = 0; $c <= $colno + 1; $c++) {
+                if ($r == $ordletstart - 1 || $r == $ordletend + 1) {
+                    // Header/footer row
+                    if ($c == 0 || $c == $colno + 1) {
+                        echo '      ';  // Corner space
+                    } else {
+                        echo str_pad($c, 6, ' ', STR_PAD_BOTH);  // Column number
+                    }
+                } else if ($c == 0 || $c == $colno + 1) {
+                    // Header/footer column
+                    if ($c == 0) {
+                        echo '  ' . chr($r) . '   ';  // This is where I figured out how to space the row headers correctly
+                    } else {
+                        echo '   ' . chr($r) . '  ';  // This is for the right side
+                    }
+                } else {
+                    // I'm tinkering with spacing here to get things to look better
+                    // But this is whe cells get built
+                    echo str_pad($ary[$r][$c], 6, ' ', STR_PAD_BOTH);
+                }
+            }
+            echo "\n";  // Made a new line after each row
+        }
+        echo '</pre>';
+
+        echo '<br><br><br>';
+        echo 'The number of elements in $ary: ' . count($ary) . "\n<br>";
+        echo 'The rec-total of elements: ', count($ary, COUNT_RECURSIVE), "\n<br>";
+    }
+    ?>
+</body>
+
+</html>
